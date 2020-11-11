3f5502139f6ddf8bbbaee9dafbb3b0e92b5db2fa
Andre Alan Alvesm <andrealansp@hotmail.com>
2020-11-10 21:22:48 -0300

    sem variavel de ambiente

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const db = {};
 db.mongoose = mongoose;
-db.url = process.env.MONGODB;
+db.url =
+  'mongodb+srv://andrealves:S@lmos2300@andrealves.ykvuo.mongodb.net/gradesApi?retryWrites=true&w=majority';
 
 export { db };
